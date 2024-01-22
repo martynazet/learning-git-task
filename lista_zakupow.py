a86be1f91b5465cbdeae3672d2d075f80e8345f1
Martyna Zawór <mart.zaw.95@gmail.com>
2024-01-22 20:01:15 +0100

    Dict items print style changed

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,4 +1,4 @@
 zakupy = {'Piekarnia':['bułka', 'chleb', 'drożdżowka'], 'Warzywniak': ['pomidor', 'sałata', 'cukinia']}
 
 for key, value in zakupy.items():
-    print(f'Idę do {key} żeby kupić {[values for values in value ]}')
+    print(f'Idę do {key.title()} żeby kupić {[values.title() for values in value ]}')
