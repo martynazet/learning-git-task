1e80cc10e68c05d2d222e3b99e729d57355c7c4e
Martyna Zawór <mart.zaw.95@gmail.com>
2024-01-22 20:04:06 +0100

    Add products sum

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,4 +1,7 @@
 zakupy = {'Piekarnia':['bułka', 'chleb', 'drożdżowka'], 'Warzywniak': ['pomidor', 'sałata', 'cukinia']}
 
+suma = 0
 for key, value in zakupy.items():
     print(f'Idę do {key.title()} żeby kupić {[values.title() for values in value ]}')
+    suma += len(value)
+print(f'W sumie kupuję {suma} produktów.')   
