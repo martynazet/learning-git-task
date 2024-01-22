292aadd9bea59b97286e0e43045a8932a056c03b
Martyna Zawór <mart.zaw.95@gmail.com>
2024-01-22 19:56:43 +0100

    Print keys with values

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,1 +1,4 @@
 zakupy = {'Piekarnia':['bułka', 'chleb', 'drożdżowka'], 'Warzywniak': ['pomidor', 'sałata', 'cukinia']}
+
+for key, value in zakupy.items():
+    print(f'Idę do {key} żeby kupić {[values for values in value ]}')
