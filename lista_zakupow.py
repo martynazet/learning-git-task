58d69c172a9dcc66ef2a614cfe975dc6abab8821
Martyna Zawór <mart.zaw.95@gmail.com>
2024-01-22 20:59:21 +0100

    add bonus

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -5,3 +5,6 @@ for key, value in zakupy.items():
     print(f'Idę do {key.title()} żeby kupić {[values.title() for values in value ]}')
     suma += len(value)
 print(f'W sumie kupuję {suma} produktów.')   
+
+print("******")
+print("Miłego dnia i smacznej kawusi")
